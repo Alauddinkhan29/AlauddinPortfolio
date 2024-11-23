015e56e35ce5d21c4ad9c9fdde57e0146b74e8ee
alauddinkhan29 <alauddinkhan29@gmail.com>
2024-11-23 11:50:42 +0530

    Initial Commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View } from 'react-native'
+import React from 'react'
+import Routes from './Src/navigation/Routes'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Routes />
+    </View>
+  )
+}
+
+export default App
+
